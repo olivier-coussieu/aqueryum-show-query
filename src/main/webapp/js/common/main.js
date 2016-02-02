57764027bbe618812e6b4ebb8c3588b3692c8780
olivier-coussieu <olivier.coussieu@atos.net>
2016-02-02 16:12:22 +0100

    chore(webapp):rename view into templates

--- src/main/webapp/js/common/main.js
+++ src/main/webapp/js/common/main.js
@@ -5,7 +5,7 @@ var app_name = (path_name) ? path_name.split('/')[1] : "";
 var aShowQApp = angular.module('aShowQApp', ['ngRoute']);
 
 aShowQApp.config(function ($routeProvider) {
-    $routeProvider.when('/list',            {templateUrl: 'view/list.html',     controller: 'DancerListCtrl'});
+    $routeProvider.when('/list',            {templateUrl: 'templates/list.html',     controller: 'DancerListCtrl'});
     $routeProvider.otherwise({redirectTo: '/list'});
 });
 
